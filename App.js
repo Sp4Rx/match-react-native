fdd1f4e943bcf41a391eb9f745db2cf76eb260fd
Sp4Rx <heysuvajit@gmail.com>
2020-07-06 02:38:32 +0530

    OnClick States added

--- App.js
+++ App.js
@@ -3,11 +3,23 @@ import React from 'react';
 import { StyleSheet, FlatList, View } from 'react-native';
 import { Header } from './components/header.js';
 import { Status } from './components/status.js';
-import { Card } from './components/card.js';
+import { Card, CardStates } from './components/card.js';
 
 export default function App() {
   const DATA = [1, 1, 2, 2];
 
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+    id => {
+      const newSelected = new Map(selected);
+      if (selected.get(id) == null || selected.get(id) == CardStates.INITIAL) {
+        newSelected.set(id, CardStates.SELECTED);
+      }
+      setSelected(newSelected);
+    },
+    [selected],
+  );
 
   return (
     <View style={styles.container}>
@@ -21,7 +33,11 @@ export default function App() {
       <View style={{ paddingBottom: 40 }}>
         <Status title="Time Left" subTitle="0:57" />
       </View>
-      <FlatList numColumns={2} data={DATA} renderItem={({ num }) => <Card />} />
+      <FlatList
+        numColumns={2}
+        data={DATA}
+        renderItem={({ item, index }) => <Card value={item} id={index} onSelect={onSelect} state={selected.get(index)} />}
+        keyExtractor={(item, index) => index.toString()} />
     </View>
   );
 }
--- components/card.js
+++ components/card.js
@@ -1,18 +1,68 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const styles = StyleSheet.create({
-    roundedRectangle: {
+    roundedRectangleGreen: {
         borderRadius: 10,
         backgroundColor: '#388e3c',
-   flex: 1,
-   height:250,
-   margin: 4,
+        flex: 1,
+        height: 250,
+        borderWidth: 2,
+        borderColor: '#388e3c',
+        margin: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    roundedRectangleWhiteBlackBorder: {
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        flex: 1,
+        height: 250,
+        borderColor: '#000',
+        borderWidth: 2,
+        margin: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    roundedRectangleWhiteBlueBorder: {
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        flex: 1,
+        height: 250,
+        borderColor: '#03a9f4',
+        borderWidth: 2,
+        margin: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    value: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: "bold",
     },
 })
 
 function Card(props) {
-    return (<View style={styles.roundedRectangle}></View>);
+    return (
+        <TouchableOpacity
+            style={
+                props.state == null || props.state == CardStates.INITIAL ?
+                    styles.roundedRectangleGreen :
+                    props.state == CardStates.SELECTED ?
+                        styles.roundedRectangleWhiteBlackBorder :
+                        styles.roundedRectangleWhiteBlueBorder}
+            onPress={() => props.onSelect(props.id)} >
+            <View >
+                <Text style={styles.value}>{props.state == null || props.state == CardStates.INITIAL ? '' : props.value}</Text>
+            </View>
+        </TouchableOpacity>
+    );
 }
 
-export { Card };
+const CardStates = {
+    INITIAL: 'initial',
+    SELECTED: 'selected',
+    MATCHED: 'matched'
+}
+
+export { Card, CardStates };
