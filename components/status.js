cc3f22021a8abdfdbbd4667ae247c77c4b580594
Sp4Rx <heysuvajit@gmail.com>
2020-07-07 23:42:43 +0530

    Formated Status

--- components/status.js
+++ components/status.js
@@ -2,28 +2,27 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    title: {
-        color: "#263238",
-        fontSize: 16,
-        fontWeight: "bold",
-        alignSelf: "center",
-    }, subTitle: {
-        color: "#f4511e",
-        fontSize: 16,
-        fontWeight: "bold",
-        alignSelf: "center",
-    },
+  title: {
+    color: '#263238',
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  subTitle: {
+    color: '#f4511e',
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
 });
 
-
 function Status(props) {
-
-    return (
-        <View>
-            <Text style={styles.title}>{props.title}</Text>
-            <Text style={styles.subTitle}>{props.subTitle}</Text>
-        </View>
-    );
+  return (
+    <View>
+      <Text style={styles.title}>{props.title}</Text>
+      <Text style={styles.subTitle}>{props.subTitle}</Text>
+    </View>
+  );
 }
 
 export { Status };
